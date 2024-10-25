082e4f4b5ab1713f033b46b3a344dd94277ef3ff
sharith <mohdsharith@gmail.com>
2024-10-25 19:40:22 +0530

    Create i2c_master_defines.sv

---
+++ PROTOCOL_PROJECT/i2c_master_defines.sv
@@ -0,0 +1,8 @@
+`define I2C_CMD_NOP   4'b0000
+`define I2C_CMD_START 4'b0001
+`define I2C_CMD_STOP  4'b0010
+`define I2C_CMD_WRITE 4'b0100
+`define I2C_CMD_READ  4'b1000
+
+// Modelsim-ASE requires a timescale directive
+`timescale 1 ns / 1 ns
