72492191e04b342598eb366e20036c3462ecbc11
sharith <mohdsharith@gmail.com>
2024-10-26 10:06:28 +0530

    Create apb_i2c_pkg.sv

---
+++ PROTOCOL_PROJECT/apb_i2c_pkg.sv
@@ -0,0 +1,16 @@
+
+
+`include "apb_2_i2c_defines.sv"
+`include "apb_transaction.sv"
+`include "apb_generator.sv"
+`include "apb_driver.sv"
+`include "apb_monitor.sv"
+
+
+`include "i2c_transaction.sv"
+`include "i2c_monitor.sv"
+`include "apb_2_i2c_sb.sv"
+`include "apb_2_i2c_env.sv"
+`include "apb_2_i2c_test.sv"
+
+
